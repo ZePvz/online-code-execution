ea1e4365b49eee54df29cb60219a3a2f62de0bf4
ZeeshanPervez-Caramellow <zeeshanpervez8@gmail.com>
2026-08-25 10:27:43 +0530

    ProtectedRoutes bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App(){
       <Routes>
         <Route path = "/login" element={<Login/>}/>
         <Route path = "signup" element={<Signup/>}/>
-        <Route element={ProtectedRoute}>
+        <Route element={<ProtectedRoute/>}>
           <Route path = "/" element={<Workspace/>}/>
         </Route>
         
