8de5cf8f21e0e3aed3a8b5e69d5d820ef32eca31
ZeeshanPervez-Caramellow <zeeshanpervez8@gmail.com>
2026-08-25 09:38:12 +0530

    Login path bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter , Routes , Route} from 'react-router-dom';
 import Workspace from  './pages/Workspace';
-import Login from './pages/login';
+import Login from './pages/Login';
 import Signup from './pages/Signup';
 import { AuthProvider } from './utils/AuthContext';
 
